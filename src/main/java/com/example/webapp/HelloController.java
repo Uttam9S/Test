487c678f349e6b0b813ae8753d1bd6d6415b264d
root <root@Uttam1.4agusm5fntcehk0pky4vy3npae.bx.internal.cloudapp.net>
2025-03-16 11:39:01 +0000

    IT

---
+++ src/main/java/com/example/webapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.webapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello from Spring Boot!";
+    }
+}
